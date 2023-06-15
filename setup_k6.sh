1d6b0864cfe4628959d14e1a91b797cd07329f94
ramzap <61818448+ramzap@users.noreply.github.com>
2023-06-15 14:19:36 +0300

    Update setup_k6.sh
    added gsudo

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
-sudo apt-get update
-sudo apt-get install dirmngr --install-recommends
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
-echo "deb https://dl.k6.io/deb stable main" | sudo tee /etc/apt/sources.list.d/k6.list
-sudo apt-get update
-sudo apt-get install k6
+gsudo apt-get update
+gsudo apt-get install dirmngr --install-recommends
+gsudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys C5AD17C747E3415A3642D57D77C6C491D6AC1D69
+echo "deb https://dl.k6.io/deb stable main" | gsudo tee /etc/apt/sources.list.d/k6.list
+gsudo apt-get update
+gsudo apt-get install k6
